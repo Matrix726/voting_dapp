9962426575e8d59ec449ab8a1b5b1d6908e5c575
Matrix726 <ajayjanapati2004@gmail.com>
2023-07-11 01:24:55 +0530

    first commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+
+
+String infura_url = "https://sepolia.infura.io/v3/dbb834c5f0874c8b97b7f284f457400b";
+String owner_private_key = "4a246041d01698ade31f3b20fe82af72b01070de251395a6a0ffff42b5c1a6e4";
+String voter_private_key = "e772c99d34e0aacccc078b5ac28af38be15b9445dae8e4e0d60e89849311c48b";
+String contractAddress1 = "0xd9145CCE52D386f254917e481eB44e9943F39138";
